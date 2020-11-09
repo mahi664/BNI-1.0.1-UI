c223302453a5869f26a7582a2b5708a829f25773
Mahesh Ghuge <mahighuge664@gmail.com>
2020-11-09 22:41:42 +0530

    code for fetching product details

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { GstDetails } from '../add-gst/add-gst.component';
 import { Product } from '../add-product/add-product.component';
+import { ProductDet } from '../inventory/inventory.component';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +21,10 @@ export class ProductService {
 
   getGstRates(){
     return this.http.get<GstDetails[]>("http://localhost:8080/get-gst-rates");
+  }
+
+  getProductDetails(){
+    return this.http.get<ProductDet[]>("http://localhost:8080/get-inventory-details");
   }
 
 }
